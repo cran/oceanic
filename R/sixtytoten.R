b91e75ddbcdae42d48751dfecdfa00ae2d01f39b
shiao chih hao <chihhao@ofdc.org.tw>
2023-01-05 14:50:05 +0000

    version 0.1.5

--- R/sixtytoten.R
+++ R/sixtytoten.R
@@ -1,27 +1,14 @@
 #' @title sixtytoten
-#' @description This function allows you to transfer the coordinate system from sexagesimal to decimal 
-#' @param lon Input the longitude.
-#' @param lat Input the latitude.
+#' @description This function allows you to transfer the coordinate system from sexagesimal to decimal
+#' @param num Input a value of longitude or latitude.
 #' @export
 #' @import sf
 #' @import spData
 #' @examples
-#' sixtytoten(121.49,25.58)
-
-
-
-sixtytoten<-function(lon,lat){
-
-
-lon<-ifelse(lon==-180,-179,lon)
-lon<-ifelse(lon==180,179,lon)
-
-a1<-ifelse(lat>=0,(trunc(abs(lat)/5)+1)*2+71,(trunc(abs(lat)/5)+1)*2+72)
-
-a2<-ifelse(lon<0,trunc(abs(lon)/5)*2+1,trunc(abs(lon)/5)*2+2)
-
-farea<-a1*100+a2
-
-return(farea)
+#' sixtytoten(121.49)
+sixtytoten <- function(num){
+num1 <- trunc(num)
+num2 <- round((num-trunc(num))/0.6,2)
+new <- num1+num2
+return(new)
 }
-
